ddb3e95ffd82ee5f3b82b29df99ee137872acb2f
Altzenck <58193685+Altzenck@users.noreply.github.com>
2023-07-21 13:41:33 -0500

    Internal changes

--- src/main/java/me/altzenck/yml/Util.java
+++ src/main/java/me/altzenck/yml/Util.java
@@ -1,33 +1,79 @@
-package me.altzenck.yaml;
+package me.altzenck.yml;
 
-import java.util.Arrays;
+import java.io.*;
+import java.util.Map;
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
 
-public class Util {
+/**
+ * @author Altzenck
+ * @version 1.1.0
+ */
+public class Yml extends Section{
 	
-	public String join(int to, String... elements) {return join(0, to, null, elements);}
-	
-	public String join(int from, int to, String... elements) {return join(from, to, null, elements);}
+   public static Reader de;
+
+   @SuppressWarnings("unchecked")
+   private Yml(InputStream is) {
+	   yaml.putAll((Map<String,Object>) new Yaml().load(is));
+   }
    
-	public static String join(int from, int to, @Nullable String l, @Nonnull String... elements) {
-		if(l == null) l = " ";
-		String text = "", ns[] = arrayRange(from, to, elements);
-		for(int i = 0; i < ns.length; i++) 
-			text += ((i == 0)? "": l) + ns[i];
-		return text;
+   public void setDefaults(File file) {
+	   setDefaults(Yml.loadYaml(file));
+   }
+   
+   public void setDefaults(Reader reader) {
+	   setDefaults(Yml.loadYaml(reader));
+   }
+   
+   public void setDefaults(InputStream is) {
+	   setDefaults(Yml.loadYaml(is));
+   }
+   
+   public void save(File file) {
+	   DumperOptions options = new DumperOptions();
+	   options.setIndent(2);
+	   options.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+	   file.mkdirs();
+	   try {
+		StringReader in = new StringReader(new Yaml(options).dump(yaml));
+	    FileOutputStream os = new FileOutputStream(file);
+	    char[] buffer = new char[2048];
+	    int i = 0;
+		while((i = in.read(buffer, 0, buffer.length))  != -1) {
+			os.write(new String(buffer).getBytes(), 0, i);
+		}
+		os.close();
+	   } catch (IOException e) {
+		e.printStackTrace();
+	   }
+   }
+   
+   public static Yml loadYaml(File file) {
+	try {
+		return loadYaml(new FileInputStream(file));
+	} catch (Exception e) {
+		return loadYaml(new ByteArrayInputStream(new byte[0]));
 	}
-	
-	public static String[] arrayRange(int to, String... elements) {return arrayRange(0, to, elements);}
-	
-	public static String[] arrayRange(int from, int to, @Nonnull String... elements) {
-		if(to < 0) to = elements.length + to;
-		return Arrays.copyOfRange(elements, from, to);
-	}
-	
-	protected static String[] splitRange(int to, String text, String regex) {return splitRange(0, to, text, regex);}
-	
-	protected static String[] splitRange(int from, int to, @Nonnull String text, String regex) {
-		return arrayRange(from, to, text.split(regex));
-	}
+   }
+   
+   public static Yml loadYaml(Reader reader) {
+	 char[] buffer = new char[2048];
+	 int i = 0;
+	 StringBuilder sb = new StringBuilder();
+	 try {
+	   while((i = reader.read(buffer, 0, buffer.length))  != -1) {
+	   	 sb.append(buffer, 0, i);
+	   }
+	   reader.close();
+	 } catch (IOException|NullPointerException e) {sb = new StringBuilder();}
+	 return loadYaml(new ByteArrayInputStream(sb.toString().getBytes()));
+	 
+   }
+   
+   public static Yml loadYaml(@Nonnull InputStream is) {
+     return new Yml(is);
+   }
+   
 }
